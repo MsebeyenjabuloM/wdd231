0871982d32a0888a68d39fcc5546d1020d66219c
MsebeyenjabuloM <mmagubane@byupathway.edu>
2025-03-15 21:30:44 +0200

    chamber page first commit

---
+++ scripts/businesses.js
@@ -0,0 +1,26 @@
+[
+    {
+        "name": "Asante Solutions",
+        "tagline": "Innovative Digital Marketing Solutions",
+        "email": "contact@asantesolutions.com",
+        "phone": "031-555-1234",
+        "url": "https://asantesolutions.com",
+        "image": "asantesolutions.jpg"
+    },
+    {
+        "name": "Seaside Coffee",
+        "tagline": "Best Coffee in Town",
+        "email": "info@seasidecoffee.com",
+        "phone": "031-888-9876",
+        "url": "https://seasidecoffee.com",
+        "image": "seasidecoffee.jpg"
+    },
+    {
+        "name": "Zen Spa",
+        "tagline": "Experience of Relaxation",
+        "email": "contact@zenspa.com",
+        "phone": "031-222-4226",
+        "url": "https://zenspa.com",
+        "image": "zenspa.jpg"
+    }
+]
