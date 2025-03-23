e87fde3aad357c07dce353691b2e246a05419b09
msebeyenjabulom <mmagubane@byupathway.edu>
2025-03-23 12:53:48 +0200

    weather api

--- Data/data.js
+++ Data/data.js
@@ -1,3 +1,6 @@
+console.log("data.js is loaded!");
+
+
 const apiKey = "2e30dcb81e7d0388b6159d9c1a070dd3";
 const city = "Durban,ZA";
 const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
