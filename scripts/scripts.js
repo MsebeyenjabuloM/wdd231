6481a93d688021edd5d8ea4fd8602e4ae27c8b9d
MsebeyenjabuloM <mmagubane@byupathway.edu>
2025-03-30 20:12:03 +0200

    Join page fixes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("timestamp").value = new Date().toISOString();
-});
+document.addEventListener('DOMContentLoaded', function() {
+    // Get form and input elements
+    const form = document.querySelector('form');
+    const firstName = document.getElementById('first-name');
+    const lastName = document.getElementById('last-name');
+    const email = document.getElementById('email');
+    const phone = document.getElementById('phone');
+    const organization = document.getElementById('organization');
+    const timestamp = document.getElementById('timestamp');
 
+    // Save form data to localStorage when form is submitted
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        localStorage.setItem("first-name", firstName.value);
+        localStorage.setItem("last-name", lastName.value);
+        localStorage.setItem("email", email.value);
+        localStorage.setItem("phone", phone.value);
+        localStorage.setItem("organization", organization.value);
+        localStorage.setItem("timestamp", new Date().toISOString());
+
+        // Redirect to thank you page after form submission
+        window.location.href = 'thankyou.html';
+    });
+});
